9f571c65bad326dc91201a4581dec3298cf896d0
Asadullah <asad.au.ullah@gmail.com>
2026-09-23 19:51:28 +0500

    Modified inngest functions to modern syntax. Previous syntax took three arguments, now it takes two.

--- config/inngest.js
+++ config/inngest.js
@@ -8,46 +8,46 @@ export const inngest = new Inngest({ id: "quickcart-next" });
 // Inngest Function to save user data to a database
 export const syncUserCreation = inngest.createFunction(
     {
-        id: 'sync-user-from-clerk'
+        id: 'sync-user-from-clerk',
+        event: 'clerk/user.created' // Moved inside the first argument
     },
-    { event: 'clerk/user.created' },
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
             _id: id,
             email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
+            name: `${first_name || ''} ${last_name || ''}`.trim(),
             imageUrl: image_url
         }
-        await connectDB
+        await connectDB()
         await User.create(userData)
     }
 )
-
 // Inngest Function to update user data in database
 export const syncUserUpdation = inngest.createFunction(
     {
-        id: 'update-user-from-clerk'
+        id: 'update-user-from-clerk',
+        event: 'clerk/user.updated'
     },
-    { event: 'clerk/user.updated' },
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
             _id: id,
             email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
+            name: `${first_name || ''} ${last_name || ''}`.trim(),
             imageUrl: image_url
         }
         await connectDB()
         await User.findByIdAndUpdate(id, userData)
     }
 )
+
 // Inngest Function to delete user from database
 export const syncUserDeletion = inngest.createFunction(
     {
-        id: 'delete-user-with-clerk'
+        id: 'delete-user-with-clerk',
+        event: 'clerk/user.deleted'
     },
-    { event: 'clerk/user.deleted' },
     async ({ event }) => {
         const { id } = event.data
 
